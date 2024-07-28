43654ce139cf0aa9a068fc0f4572d13cb3f23f22
Ivan Zubkov <thundiverter@gmail.com>
2024-07-28 18:05:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import NavigationEditorView from './views/navigationEditorView/NavigationEditorView';
-import Footer from './components/footer/Footer';
 
 function App() {
   const [active, setActive] = useState("Navigation");
